6bcb359f81258352a0b74921b6b916c8b5089e7b
veerabala <imveerabala@gmail.com>
2024-07-05 15:54:41 +0530

    2nd check

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import Home from "./components/pages/Home.jsx"
 import ProductDetails from "./components/products/ProductDetails.jsx";
 import Cart from "./components/cart/Cart.jsx";
-import Login from "./components/pages/Login.jsx"
+// import Login from "./components/pages/Login.jsx"
 import { Routes, Route, } from "react-router-dom";
 import Footer from "./components/common/footer/Footer.jsx"
 
 function App() {
 
-  const getEmail = localStorage.getItem("emailData")
-  const getPassword = localStorage.getItem("passwordData")
+  // const getEmail = localStorage.getItem("emailData")
+  // const getPassword = localStorage.getItem("passwordData")
   const container = {
     maxWidth: '90%',
     display: 'flex',
@@ -36,7 +36,7 @@ function App() {
       { getEmail&&getPassword ?  <Footer />:''} */}
        <div style={container}>
             <Routes>
-            <Route path={"/"} element={<Login />} />
+            {/* <Route path={"/"} element={<Login />} /> */}
                 <Route path="/home" element={<Home />} />
                 <Route path="/product/:id" element={<ProductDetails />} />
                 <Route path="/cart" element={<Cart />} /> 
