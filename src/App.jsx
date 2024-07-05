c67b9148f97f7b6902b714af46d559e01b840e3d
veerabala <imveerabala@gmail.com>
2024-07-05 15:57:01 +0530

    checkfalse

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import Home from "./components/pages/Home.jsx"
 import ProductDetails from "./components/products/ProductDetails.jsx";
 import Cart from "./components/cart/Cart.jsx";
-// import Login from "./components/pages/Login.jsx"
+import Login from "./components/pages/Login.jsx"
 import { Routes, Route, } from "react-router-dom";
 import Footer from "./components/common/footer/Footer.jsx"
 
 function App() {
 
-  // const getEmail = localStorage.getItem("emailData")
-  // const getPassword = localStorage.getItem("passwordData")
+  const getEmail = localStorage.getItem("emailData")
+  const getPassword = localStorage.getItem("passwordData")
   const container = {
     maxWidth: '90%',
     display: 'flex',
@@ -19,7 +19,7 @@ function App() {
   };
   return (
     <>
-    {/* <div style={container}>
+    <div style={container}>
             <Routes>
             <Route path={"/"} element={<Login />} />
                 {
@@ -33,16 +33,7 @@ function App() {
                 }
             </Routes>
       </div> 
-      { getEmail&&getPassword ?  <Footer />:''} */}
-       <div style={container}>
-            <Routes>
-            {/* <Route path={"/"} element={<Login />} /> */}
-                <Route path="/home" element={<Home />} />
-                <Route path="/product/:id" element={<ProductDetails />} />
-                <Route path="/cart" element={<Cart />} /> 
-            </Routes>
-      </div> 
-      <Footer />
+      { getEmail&&getPassword ?  <Footer />:''}
     </>
   );
 }
